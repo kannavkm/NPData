01135432ca7c55a1b39a5e08757d61dbf13e11e5
Kannav Mehta <kmkannavkmehta@gmail.com>
2020-10-04 09:43:00 +0530

    few changes here and there.
    Signed-off-by: Kannav Mehta <kmkannavkmehta@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess as sp
+
 import pymysql.cursors
 
 from local import config
@@ -21,6 +23,24 @@ while True:
         else:
             print("Failed to connect")
         tmp = input(">>>")
+
+        with con.cursor() as cur:
+            while (1):
+                tmp = sp.call('clear', shell=True)
+                # Here taking example of Employee Mini-world
+                print("1. Hire an Employee ")
+                print("2. Add a new Department")
+                print("3. Hire a User")
+                print("4. Add a new Service")
+                print("8. Add a feedback for a service")
+                print("9. Add a feedback for a feature")
+                print("10. Add timings for service")
+                ch = int(input("Enter choice> "))
+                tmp = sp.call('clear', shell=True)
+                if ch >= 100:
+                    break
+                else:
+                    tmp = input("Enter any key to CONTINUE> ")
 
     except pymysql.err.OperationalError as e:
         print(e.args[0], ':', e.args[1])
