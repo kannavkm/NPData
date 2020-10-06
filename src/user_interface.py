b79780d8b67ba32c9730a2a0dcd1a54c3ccad9ce
Kannav Mehta <kmkannavkmehta@gmail.com>
2020-10-06 17:18:02 +0530

    :

--- src/user_interface.py
+++ src/user_interface.py
@@ -400,74 +400,73 @@ class UserInterface:
 
     def give_feature_feedback(self):
         pass
-        # try:
-        #     q = []
-        #     while True:
-        #         print_header('Feature Feedback')
-        #         national_list = "SELECT unit_code, name from National_Park"
-        #         rows = self.db.get_result(national_list)
-        #         print('Here are the National Parks')
-        #         i = 0
-        #         for row in rows:
-        #             print('{}. {}'.format(i + 1, row['name']))
-        #             i += 1
-        #         unit_code = int(
-        #             input('Enter the corresponding number of the National Park you want to provide feedback for:'))
-        #         if not syntax.validate_range(unit_code, 1, len(rows)):
-        #             perror('invalid Input')
-        #             return
-        #         target_np = rows[unit_code - 1]
-        #
-        #         print_header('Feature Feedback')
-        #         feature_list = "SELECT service_id, name, description from Services where provided_by ='{}' ".format(
-        #             target_np['unit_code'])
-        #         rows = self.db.get_result(service_list)
-        #         if len(rows) == 0:
-        #             print("Sorry we couldn't find services that are available,",
-        #                   "You will now be transferred back to the main screen")
-        #             time.sleep(2.5)
-        #             continue
-        #
-        #         print("Here's a list of services offered by", target_np['name'])
-        #         i = 0
-        #         for row in rows:
-        #             print('{}. {}: \n {}'.format(i + 1, row['name'], row['description']))
-        #             i += 1
-        #
-        #         service_code = int(
-        #             input('Enter the corresponding number of the service you want to provide feedback for:'))
-        #         if not syntax.validate_range(service_code, 1, len(rows)):
-        #             perror('invalid Input')
-        #             return
-        #         target_service = rows[service_code - 1]
-        #
-        #         rating = int(input('Enter the rating you would like to give to {}'.format(target_service['name'])))
-        #
-        #         if not syntax.validate_range(rating, 1, 5):
-        #             perror('invalid Input')
-        #             return
-        #
-        #         print("Enter your remarks if any")
-        #         remarks = input()
-        #
-        #         qq = "INSERT INTO Service_Feedback(user_id, service_id, rating, remarks, date)" \
-        #              "VALUES({},{},{},'{}',NOW())".format(self.current_user.user_id, target_service['service_id'],
-        #                                                   rating, remarks)
-        #
-        #         q.append(qq)
-        #
-        #         print('Do you want to continue give feedback(y/n)')
-        #         ans = input()
-        #         if ans.lower() != 'y':
-        #             break
-        #
-        #     self.db.execute_query(q)
-        #     psuccess("Congratulations your feedback was recorded")
-        #     time.sleep(2.5)
+        try:
+            q = []
+            while True:
+                print_header('Feature Feedback')
+                national_list = "SELECT unit_code, name from National_Park"
+                rows = self.db.get_result(national_list)
+                print('Here are the National Parks')
+                i = 0
+                for row in rows:
+                    print('{}. {}'.format(i + 1, row['name']))
+                    i += 1
+                unit_code = int(
+                    input('Enter the corresponding number of the National Park you want to provide feedback for:'))
+                if not syntax.validate_range(unit_code, 1, len(rows)):
+                    perror('invalid Input')
+                    return
+                target_np = rows[unit_code - 1]
 
-        # except Exception as q:
-        #     print(type(q))
-        #     time.sleep(5)
+                print_header('Feature Feedback')
+                feature_list = "SELECT F.feature_name,  
+                rows = self.db.get_result(service_list)
+                if len(rows) == 0:
+                    print("Sorry we couldn't find features that are available,",
+                          "You will now be transferred back to the main screen")
+                    time.sleep(2.5)
+                    continue
+
+                print("Here's a list of features in", target_np['name'])
+                i = 0
+                for row in rows:
+                    print('{}. {}: \n {}'.format(i + 1, row['name'], row['description']))
+                    i += 1
+
+                service_code = int(
+                    input('Enter the corresponding number of the service you want to provide feedback for:'))
+                if not syntax.validate_range(service_code, 1, len(rows)):
+                    perror('invalid Input')
+                    return
+                target_service = rows[service_code - 1]
+
+                rating = int(input('Enter the rating you would like to give to {}'.format(target_service['name'])))
+
+                if not syntax.validate_range(rating, 1, 5):
+                    perror('invalid Input')
+                    return
+
+                print("Enter your remarks if any")
+                remarks = input()
+
+                qq = "INSERT INTO Service_Feedback(user_id, service_id, rating, remarks, date)" \
+                     "VALUES({},{},{},'{}',NOW())".format(self.current_user.user_id, target_service['service_id'],
+                                                          rating, remarks)
+
+                q.append(qq)
+
+                print('Do you want to continue give feedback(y/n)')
+                ans = input()
+                if ans.lower() != 'y':
+                    break
+
+            self.db.execute_query(q)
+            psuccess("Congratulations your feedback was recorded")
+            time.sleep(2.5)
+
+        except Exception as q:
+            print(type(q))
+            time.sleep(5)
 
     def loop(self):
         print("Are you an existing user? (y/n): ")
